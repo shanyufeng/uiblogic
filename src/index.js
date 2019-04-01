37d22aba844bcef878a2c4b60bf2f522a7930719
shanyufeng <shanyufeng@bonc.com.cn>
2019-04-01 15:46:55 +0800

    测试注释

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter} from 'react-router-dom';
 import App from './App';
 import './css/common.css'
-import * as serviceWorker from './serviceWorker';
+//import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
     //BrowserRouter 只包含一个元素
@@ -14,4 +14,4 @@ ReactDOM.render(
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+//serviceWorker.unregister();
